5a3c838c83fd79b6d17fc4e87a51808666d5f6c6
Akiva Zonenfeld <akivazo1@gmail.com>
2024-09-30 20:49:07 +0300

    init

---
+++ server/feedback_api.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from uuid import uuid4
+from pydantic import BaseModel, ValidationError
+from typing import Type
+
+class FeedbackSchema(BaseModel):
+    id: str
+    idea_id: str
+    owner_id: str
+    content: str
+
+server = Flask(__name__)
+collection = None
+
+def set_mongo_client(mongo_client: MongoClient):
+    global collection
+    collection = mongo_client.get_database("brain_storm").get_collection("feedbacks")
+
+@server.route("/")
+def root():
+    return "Feedback Api"
+
+def validate_json_schema(json: dict, cls: Type):
+    # validate that the accepted json is containing all the data nedded
+    instance = None
+    try:
+        instance = cls(**json)
+    except ValidationError as e:
+        return None, e.json()
+    return instance.__dict__, ""
+
+@server.route("/feedback", methods=["POST"])
+def add_feedback():
+    data = request.get_json()
+    id = str(uuid4())
+    data["id"] = id
+    feedback_dict, error_message = validate_json_schema(data, FeedbackSchema)
+    if not feedback_dict:
+        return jsonify({"error": error_message}), 400
+    idea_id = feedback_dict["idea_id"]
+    del feedback_dict["idea_id"]
+    collection.update_one(
+        {"idea_id": idea_id}, 
+        {"$push": {"feedbacks": feedback_dict}},
+        upsert=True)
+    return jsonify({"id": id}), 201
+
+
+@server.route("/feedbacks/<idea_id>", methods=["GET"])
+def get_feedbacks(idea_id: str):
+    feedbacks = collection.find_one({"idea_id": idea_id}, {"_id": 0, "feedbacks": 1})
+
+    if feedbacks:
+        return jsonify(feedbacks), 302
+    return jsonify({"error": f"Can't found feedbacks for idea with id: {idea_id}"}), 404
+
+@server.route("/feedback/<idea_id>/<feedback_id>", methods=["DELETE"])
+def delete_feedback(idea_id, feedback_id: str):
+    collection.update_one({"idea_id": idea_id}, {"$pull": {"feedbacks": {"id": feedback_id}}})
+    return "", 204
+
+@server.route("/feedbacks/<idea_id>", methods=["DELETE"])
+def delete_idea_feedbacks(idea_id: str):
+    collection.delete_one({"idea_id": idea_id})
+    return "", 204
+
+if __name__ == "__main__":
+    import os, dotenv
+    dotenv.load_dotenv()
+    mongo_client = MongoClient(os.environ["FEEDBACK_MONGODB_URI"])
+    set_mongo_client(mongo_client)
+    server.run(debug=True, port=5000)
+    mongo_client.close()
---
+++ server/tests/test_feedback_api.py
@@ -0,0 +1,102 @@
+import pytest
+
+from flask.testing import FlaskClient
+from mongomock import MongoClient
+from ..feedback_api import server, set_mongo_client
+
+@pytest.fixture
+def client():
+    # Create a mock MongoDB client
+    mock_mongo_client = MongoClient()
+    set_mongo_client(mock_mongo_client)
+    server.config['TESTING'] = True
+    # Set up Flask test client
+    client = server.test_client()
+    yield client
+    mock_mongo_client.close()
+
+
+def test_add_feedback(client: FlaskClient):
+    response = client.post("/feedback", json={"idea_id": "123", "owner_id": "akiva1", "content": "feedback"})
+
+    assert response.status_code == 201
+    assert "id" in response.get_json()
+
+def test_add_feedback_missing_data(client: FlaskClient):
+    response = client.post("/feedback", json={"owner_id": "akiva1", "content": "feedback"})
+
+    assert response.status_code == 400
+    assert "error" in response.get_json()
+
+def test_get_feedbacks(client: FlaskClient):
+    id1 = client.post("/feedback", json={"idea_id": "123", "owner_id": "akiva1", "content": "feedback"}).get_json()["id"]
+    id2 = client.post("/feedback", json={"idea_id": "123", "owner_id": "akiva2", "content": "feedback"}).get_json()["id"]
+    id3 = client.post("/feedback", json={"idea_id": "345", "owner_id": "akiva3", "content": "feedback"}).get_json()["id"]
+
+    response = client.get("/feedbacks/123")
+
+    assert response.status_code == 302
+    feedbacks = response.get_json()["feedbacks"]
+
+    assert feedbacks == [{"owner_id": "akiva1", "content": "feedback", "id": id1}, {"owner_id": "akiva2", "content": "feedback", "id": id2}]
+
+    response = client.get("/feedbacks/345")
+
+    assert response.status_code == 302
+    feedbacks = response.get_json()["feedbacks"]
+
+    assert feedbacks == [{"owner_id": "akiva3", "content": "feedback", "id": id3}]
+
+def test_delete_feedback(client: FlaskClient):
+    id1 = client.post("/feedback", json={"idea_id": "123", "owner_id": "akiva1", "content": "feedback"}).get_json()["id"]
+    id2 = client.post("/feedback", json={"idea_id": "123", "owner_id": "akiva2", "content": "feedback"}).get_json()["id"]
+    client.post("/feedback", json={"idea_id": "345", "owner_id": "akiva3", "content": "feedback"}).get_json()["id"]
+    
+    response = client.get("/feedbacks/123")
+
+    feedbacks = response.get_json()["feedbacks"]
+
+    assert feedbacks == [{"owner_id": "akiva1", "content": "feedback", "id": id1}, {"owner_id": "akiva2", "content": "feedback", "id": id2}]
+
+    response = client.delete(f"/feedback/123/{id1}")
+
+    assert response.status_code == 204
+
+    response = client.get("/feedbacks/123")
+
+    assert response.status_code == 302
+
+    feedbacks = response.get_json()["feedbacks"]
+
+    assert feedbacks == [{"owner_id": "akiva2", "content": "feedback", "id": id2}]
+
+def test_delete_feedbacks(client: FlaskClient):
+    client.post("/feedback", json={"idea_id": "123", "owner_id": "akiva1", "content": "feedback"})
+    client.post("/feedback", json={"idea_id": "123", "owner_id": "akiva2", "content": "feedback"})
+    client.post("/feedback", json={"idea_id": "345", "owner_id": "akiva3", "content": "feedback"})
+    client.post("/feedback", json={"idea_id": "345", "owner_id": "akiva4", "content": "feedback"})
+
+    response = client.get("/feedbacks/123")
+
+    assert response.status_code == 302
+    feedbacks = response.get_json()["feedbacks"]
+
+    assert len(feedbacks) == 2
+
+    response = client.delete("/feedbacks/123")
+
+    assert response.status_code == 204
+
+    response = client.get("/feedbacks/123")
+
+    assert response.status_code == 404
+
+    assert "error" in response.get_json()
+
+    response = client.get("/feedbacks/345")
+
+    assert response.status_code == 302
+    feedbacks = response.get_json()["feedbacks"]
+
+    assert len(feedbacks) == 2
+
